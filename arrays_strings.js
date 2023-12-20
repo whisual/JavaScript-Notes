da2fd19a907a8958f4d0a74b36a70eeef151bfda
Vishal Sharma <102051680+whisual@users.noreply.github.com>
2023-12-20 18:05:20 +0530

    Add files via upload

---
+++ arrays_strings.js
@@ -0,0 +1,61 @@
+// ------------------------------------------------ ARRAY METHODS ------------------------------------------------------//
+
+
+// let arr = [1, 3, 5, 7, 9, 2, 4, 6, 8,]
+
+// console.log(arr.map((num) => { return num * 10 }))   it changes all the elements of an array in a desired way
+
+// console.log(arr.filter((num) => { return num % 2 == 0 }))  it removes the desired elements from an array
+
+// console.log(arr.reduce((acc, num) => { return acc + num }, 0))  it adds all the elements of an array
+
+// arr.push("Puneet") it adds an element at the ending of an array
+// console.log(arr);
+
+// arr.pop()   it removes an element at the ending of an array
+// console.log(arr);
+
+// arr.shift()   it removes an element from the starting of an array
+// console.log(arr);
+
+// arr.unshift(1)   it adds the element in the starting of an array
+// console.log(arr);
+
+// let arr2 = arr.join()   it converts array into a string
+// console.log(arr2);
+
+// console.log(arr.length); it shows the length of an array
+
+// let arr3 = arr.slice(1, 5)   it slices the array and returns the elements from given indexes
+// console.log(arr3);
+
+// let arr4 = arr.splice(1, 5)   it slices the array returns the elements from given indexes and deletes the elements from original array
+// console.log(arr4);    here it returned from the given indexes
+// console.log(arr);     here it deleted them from original array
+
+
+//-------------------------------------------- STRING METHODS ------------------------------------------------------------//
+
+
+// let str = "   VishalSharma   "
+
+// console.log(str.length)    it shows the length of a String
+
+// console.log(str.toLowerCase())  it convetrs the string to lowercase
+// console.log(str.toUpperCase());  it converts the string to uppercase
+
+// console.log(str.replace("i", "x"))   it changes the specific element in a string
+
+// console.log(str.charAt(0));    it shows the character at asked position
+
+
+// let str2 = "SmartBoy"    it adds two strings i a paragraph
+// console.log(`My name is ${str} and i am a ${str2}`);
+
+// let str3 = str.slice(0, 6)     it slices the string from given indexes
+// console.log(str3);
+
+// console.log(str);
+// console.log(str.trim());   it trims the unwanted spaces from starting and ending
+
+
---
+++ math_date.js
@@ -0,0 +1,50 @@
+//---------------------------------------------------- MATHS -------------------------------------------------------------------//
+
+
+// console.log(Math.abs(-4));    it converts the negative values into positive
+
+// console.log(Math.round(4.7));  it round off the given value
+
+// console.log(Math.ceil(4.3));    it round off to the bigger value
+
+// console.log(Math.floor(4.7));  it round off to the smaller value
+
+// console.log(Math.min(2, 4, 6, 8));   it gives the minimum number from the set of numbers
+
+// console.log(Math.max(2, 4, 6, 8));   it gives the maximum number from the set of numbers
+
+// console.log(Math.random());      it gives the random number between 0 to 1
+
+// console.log(Math.random() * 10 + 1 );   by this method, we get random number bigger than 1
+
+// const min = 10                          by this method we get random number between two numbers
+// const max = 20
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+//-------------------------------------------------- DATE --------------------------------------------------------------//
+
+
+// const myDate = new Date()
+
+// console.log(myDate.getTime())
+
+// console.log(myDate.getFullYear());
+
+// console.log(myDate.getMonth() + 1);
+
+// console.log(myDate.getDate());
+
+// console.log(myDate.getDay());
+
+// console.log(myDate.getHours());
+
+// console.log(myDate.getMinutes());
+
+// console.log(myDate.getSeconds());
+
+// console.log(myDate.toISOString());
+
+// console.log(myDate.toUTCString());
+
+// console.log(myDate.getTimezoneOffset());
