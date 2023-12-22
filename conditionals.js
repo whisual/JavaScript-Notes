8dd75c18e5a3157ed3105dcb545329fe3ec63f1a
Vishal Sharma <102051680+whisual@users.noreply.github.com>
2023-12-23 00:25:12 +0530

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,39 @@
+//--------------------------------------- Conditionals Statements ---------------------------------------//
+
+// There are two types of conditional statements
+// 1. if/else
+// 2. switch cases
+
+// if/else 👇
+
+// const checkAge = (age) => {
+//     if (age >= 18) {
+//         console.log("You are eligible");
+//     }
+//     else {
+//         console.log("You are not eligible");
+//     }
+// }
+// checkAge(22);
+
+// switch statements 👇
+
+// const checkAge = (age) => {
+//     switch (true) {
+//         case age > 18:
+//             console.log("You are eligible");
+//             break;
+
+//         case age === 18:
+//             console.log("On point");
+//             break;
+
+//         case age < 18:
+//             console.log("You are not eligible");
+//             break;
+
+//         default:
+//             console.log("Please enter valid age");
+//     }
+// }
+// checkAge(18);
