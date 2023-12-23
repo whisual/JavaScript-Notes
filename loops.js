73d7cc90d8fb8255ca60cb47b1c11f179bf9b42e
Vishal Sharma <102051680+whisual@users.noreply.github.com>
2023-12-24 01:11:40 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,51 @@
+//----------------------------------------- Loops -----------------------------------//
+
+
+
+// There are mainly three types of loops
+// 1. for loop
+// 2. while loop
+// 3. do while loop
+
+// basic structure of for loop 👇
+
+// const loop1 = () => {
+//     for (let i = 0; i <= 100; i++) {
+//         console.log(i);
+//     }
+// }
+// loop1();
+
+// Here in line no. 13 first we initialized i (let i=0) then we gave it ending parameter (i<=100) then we increment it (i++)
+
+
+// basic structure of while loop 👇
+
+// const loop2 = () => {
+//     let i = 0;
+//     while (i <= 100) {
+//         console.log(i);
+//         i++;
+
+//     }
+// }
+// loop2();
+
+// Here in while loop, first we initialized the value in line 25 then we started while loop and gave ending parameter in line 26
+// then after printing in line 27 we add the increment on the vlaue in line 28.
+
+
+// basic structure of do while loop 👇
+
+// const loop3 = () => {
+//     let i = 0;
+//     do {
+//         console.log(i);
+//         i++;
+//     }
+//     while (i <= 100)
+// }
+// loop3();
+
+// Here in do while loop, first we initialized the value in line 41 the we gave the task of printing the value in line 43 and then
+// incrementing it in line 44 then we gave the ending parameter in line 46.
