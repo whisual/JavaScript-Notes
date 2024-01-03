43ed19b48df5d06eec0362d7e398136f3e677aa4
Vishal Sharma <102051680+whisual@users.noreply.github.com>
2024-01-03 23:30:28 +0530

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,29 @@
+//----------------------------------------- Fetching API ---------------------------//
+
+
+
+// const url = "https://api.quotable.io/random"
+
+//--------------------- .then method -------------------//
+
+
+// const fetchapi = fetch(url)
+// fetchapi.then((res) => {
+//     return res.json()
+// })
+//     .then((data) => {
+//         console.log(data);
+//     })
+
+
+//-------------------- Async Await method ---------------//
+
+
+// async function fetchApi(url) {
+
+//     const res = await fetch(url)
+//     const data = await res.json()
+//     console.log(data);
+// }
+// fetchApi(url);
+
